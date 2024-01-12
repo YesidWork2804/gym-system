9b53a7904bfd57a6a353738b04167853b8a23d18
YesidWork2804 <yesidwork2804@gmail.com>
2024-01-12 17:16:24 -0500

    create folder ui, components myButton and myIcon

---
+++ src/app/ui/design_system/my-button/my-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-button.component.html',
+  styles: ``
+})
+export class MyButtonComponent {
+
+}
---
+++ src/app/ui/design_system/my-icon/my-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-icon.component.html',
+  styles: ``
+})
+export class MyIconComponent {
+
+}
